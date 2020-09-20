fb31d79a1753918ff57b5c0db2934df4b6307811
Aaron <mohammad.chegini@hotmail.com>
2020-09-20 10:39:09 +0430

    Some minute advances

--- hello.js
+++ hello.js
@@ -46,7 +46,7 @@ window.frames[2].frames["Master"].frames["Form_Body"].document.querySelector(
 
 window.frames[2].frames["Master"].frames["Form_Body"].document.querySelector(
   "#F01952"
-).value = "232";
+).value = "078";
 
 window.frames[2].frames["Master"].frames["Form_Body"].document.querySelector(
   "#F51707"
@@ -55,3 +55,34 @@ window.frames[2].frames["Master"].frames["Form_Body"].document.querySelector(
 window.frames[2].frames["Master"].frames["Form_Body"].document
   .querySelector("#DoCh")
   .click();
+
+/* --------------------------------------------------------------  */
+var len = window.frames[2].frames["Master"].frames[
+  "Form_Body"
+].document.getElementsByClassName("TableDataRow").length;
+
+for (var i = 0; i < len; i++) {
+  var obj = window.frames[2].frames["Master"].frames[
+    "Form_Body"
+  ].document.getElementsByClassName("TableDataRow")[i];
+
+  if (obj.cells[4].innerHTML == "23")
+    if (obj.cells[3].innerHTML == "01")
+      if (obj.cells[2].innerHTML == "016") obj.click();
+}
+
+var len2 = window.frames[2].frames["Master"].frames[
+  "Form_Body"
+].document.getElementsByClassName("Inserted").length;
+for (var i = 0; i < len2; i++) {
+  var obj = window.frames[2].frames["Master"].frames[
+    "Form_Body"
+  ].document.getElementsByClassName("Inserted")[i];
+
+  if (obj.cells[5].innerHTML == "23")
+    if (obj.cells[4].innerHTML == "01")
+      if (obj.cells[3].innerHTML == "016")
+        obj.cells[2].childNodes[0].value = "10";
+}
+
+window.frames[2].frames["Commander"].document.querySelector("#IM13_Do").click();
